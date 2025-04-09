d117b6ec2db27d9c9760fcfc27d80220fdedc4d5
Marzena Jaworska <marzenajaworskagit@gmail.com>
2025-04-09 20:41:46 +0200

    Moved the code for setting up the scene to separate class. Still works and draws red boc

---
+++ src/DemoScene.ts
@@ -0,0 +1,23 @@
+import * as THREE from 'three';
+
+export default class DemoScene extends THREE.Scene
+{
+    create()
+    {
+        // create a prop to see something on a screen 
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshPhongMaterial({color: 0xFF0000});
+        const prop = new THREE.Mesh(geometry, material);
+        prop.position.set(0, 0, -4); 
+
+        // add prop to the scene
+        this.add(prop);
+
+        // add light and position it above and between camera and a prop
+        const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+        light.position.set(0, 5, 1);
+
+        // add light to the scene
+        this.add(light);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'  // todo: remove?
 import * as THREE from 'three';
+import DemoScene from './DemoScene';
 
 // window params
 const width = window.innerWidth;
@@ -20,23 +21,8 @@ mainCamera.position.set(0, 2, 4);
 mainCamera.lookAt(0, 0, 0);
 
 // add a scene
-const scene = new THREE.Scene();
-
-// create a prop to see something on a screen 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshPhongMaterial({color: 0xFF0000});
-const prop = new THREE.Mesh(geometry, material);
-prop.position.set(0, 0, -4); 
-
-// add prop to the scene
-scene.add(prop);
-
-// add light and position it above and between camera and a prop
-const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-light.position.set(0, 5, 1);
-
-// add light to the scene
-scene.add(light);
+const scene = new DemoScene();
+scene.create();
 
 // pass scene and camera to renderer and render the scene
 renderer.render(scene, mainCamera);
