a65fd0a69c548a93b2c6667424e4519cde729e0a
Marzena Jaworska <marzenajaworskagit@gmail.com>
2025-04-10 01:21:46 +0200

    Moved loading of an object to DemoScene to clean up main. Added Async loading, Encapsulated adding simple box to easily reuse it. Added Recast on mouse move to dectect pointed object. Added market indicator to verify the collision. Added gizmo that attaches to pointed object.

--- src/DemoScene.ts
+++ src/DemoScene.ts
@@ -1,8 +1,12 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const gltfLoader = new GLTFLoader(); //todo: find a better place for it
+//export let loadedObject: Object;
 
 export default class DemoScene extends THREE.Scene
 {
-    create()
+    addFloor()
     {   
         // create a prop to see something on a screen 
         const geometry = new THREE.BoxGeometry();
@@ -11,13 +15,63 @@ export default class DemoScene extends THREE.Scene
         floor.position.set(0, 0, 0); 
         floor.scale.set(10, 0.1, 10);
         // add prop to the scene
-        this.add(floor);
+        this.add(floor);       
+    }
+    
+    addBox(color: THREE.ColorRepresentation, position: THREE.Vector3, scale: THREE.Vector3) : THREE.Object3D
+    {   
+        // create a prop to see something on a screen 
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshPhongMaterial({color: color});
+        const box = new THREE.Mesh(geometry, material);
+        box.position.set(position.x, position.y, position.z); 
+        box.scale.set(scale.x, scale.y, scale.z);
+        
+        // add prop to the scene
+        this.add(box);
+        return box;
+    }
 
-        // add light and position it above and between camera and a prop
-        const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-        light.position.set(0, 5, 1);
+    addLights()
+    {
+         // add light and position it above and between camera and a prop
+         const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+         light.position.set(0, 5, 1);
+ 
+         // add light to the scene
+         this.add(light);
+    }
 
-        // add light to the scene
-        this.add(light);
+    // todo: remove one of them loadAndAddObjectAsync or loadAndAddObject
+    async loadAndAddObjectAsync()
+    {   
+        const gltf = await gltfLoader.loadAsync('assets/Wooden_Table.glb');
+        const model = gltf.scene;
+        model.position.set(0, 0, -1);
+        model.scale.set(3,3,3);
+        this.add( model );
+    } 
+
+    loadAndAddObject()
+    {   
+        const scene = this; //todo: reconsider, passed scene to a callback function some other way
+        gltfLoader.load('assets/Wooden_Table.glb', 
+          function (gltf){ 
+              const model = gltf.scene;
+              model.position.set(0, 0, -1);
+              model.scale.set(3,3,3);
+              scene.add( model );
+          },
+        
+          // called while loading is progressing
+          function ( xhr ) {
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+          },
+
+          // called when loading has errors
+          function ( error ) {
+            console.log( 'An error happened' + error );
+          }
+        )
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './style.css'  // todo: remove?
 import * as THREE from 'three';
 import DemoScene from './DemoScene';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { TransformControls } from 'three/addons/controls/TransformControls.js';
+
 
 // window params
 const width = window.innerWidth;
@@ -15,6 +16,8 @@ const nearPlane = 0.1;
 const farPlane = 100;
 
 const colorLightBlue: THREE.ColorRepresentation = 0xADD8E6;
+const colorRed: THREE.ColorRepresentation = 0xFF0000;
+
 // create the rendered and set it to cover whole page
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
@@ -28,32 +31,58 @@ const mainCamera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPla
 mainCamera.position.set(0, 2, 4);    
 mainCamera.lookAt(0, 0, 0);
 
-// creat controlls to move camera and look around
+// creat controls to move camera and look around
 const cameraControls = new OrbitControls( mainCamera, renderer.domElement );
+
+// array to story objects we want to check against during recast
+let selectableObjects: THREE.Object3D[] = [];
 
 // add a scene
 const scene = new DemoScene();
-scene.create();
+scene.addFloor();
+scene.addLights();
+scene.loadAndAddObjectAsync();
 
-// load table model
-const gltfLoader = new GLTFLoader();
-gltfLoader.load('assets/Wooden_Table.glb', 
-  function (gltf){ 
-      const model = gltf.scene;
-      model.position.set(0, 0, -1);
-      model.scale.set(3,3,3);
-      scene.add( model );
-  },
+// Transform Controls for Gizmos
+const transformControls = new TransformControls(mainCamera, renderer.domElement);
+transformControls.setMode('translate');
+scene.add(transformControls.getHelper());
 
-  // called while loading is progressing
-  function ( xhr ) {
-    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-  },
-  // called when loading has errors
-  function ( error ) {
-    console.log( 'An error happened' + error );
+//Handling selection of an object
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+// there's probably a way to define zero vector
+const markerProp = scene.addBox(colorRed, new THREE.Vector3(0,0,0), new THREE.Vector3(0.01,10.0,0.01));
+scene.add(markerProp);
+
+const box = scene.addBox(colorRed, new THREE.Vector3(2,2,-3), new THREE.Vector3(1,1,1));
+scene.add(box);
+selectableObjects.push(box);
+//todo: would be nice to get rid of any
+ function onPointerMove( event: any ) {
+
+	// calculate pointer position in normalized device coordinates (-1 to +1) for both components
+  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  
+  // update the picking ray with the camera and pointer position
+	raycaster.setFromCamera( pointer, mainCamera );
+  const intersectedObjects = raycaster.intersectObjects( selectableObjects, true);
+ 
+  if( intersectedObjects.length > 0)
+  {
+    const hitObject = intersectedObjects[ 0 ].object;
+    const pos = hitObject.position;
+    markerProp.position.set(pos.x, pos.y, pos.z);
+    transformControls.attach(hitObject);
   }
-)
+  else
+  {
+    markerProp.position.set(0, 0, 0);
+    transformControls.detach();
+  }
+}
 
 // main loop of application
 function loop() {
@@ -65,8 +94,10 @@ function loop() {
 
    // pass scene and camera to renderer and render the scene
   renderer.render(scene, mainCamera);
-  
-
 }
+
+
+
+window.addEventListener( 'pointermove', onPointerMove );
 
 requestAnimationFrame(loop);
