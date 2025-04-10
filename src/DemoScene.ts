daadaf91418b402762c76337cf1802f5ff5655fc
Marzena Jaworska <marzenajaworskagit@gmail.com>
2025-04-10 07:43:47 +0200

    Added selecting object, made it possible to select the table not only box. Moved code around, Added handling for changin size of the window

--- src/DemoScene.ts
+++ src/DemoScene.ts
@@ -2,75 +2,51 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const gltfLoader = new GLTFLoader(); //todo: find a better place for it
+const gltTablePath = 'assets/Wooden_Table.glb';
 
-export default class DemoScene extends THREE.Scene
-{
-    addFloor()
-    {   
-        // create a prop to see something on a screen 
+export default class DemoScene extends THREE.Scene {
+    addBox(color: THREE.ColorRepresentation, position: THREE.Vector3, scale: THREE.Vector3): THREE.Object3D {
+        // create box
         const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
-        const floor = new THREE.Mesh(geometry, material);
-        floor.position.set(0, 0, 0); 
-        floor.scale.set(10, 0.1, 10);
-        // add prop to the scene
-        this.add(floor);       
-    }
-    
-    addBox(color: THREE.ColorRepresentation, position: THREE.Vector3, scale: THREE.Vector3) : THREE.Object3D
-    {   
-        // create a prop to see something on a screen 
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshPhongMaterial({color: color});
+        const material = new THREE.MeshBasicMaterial({ color: color });
         const box = new THREE.Mesh(geometry, material);
-        box.position.set(position.x, position.y, position.z); 
+        box.position.set(position.x, position.y, position.z);
         box.scale.set(scale.x, scale.y, scale.z);
-        
-        // add prop to the scene
+        // box.userData.randomInfo = 2; // todo: remove if not used, hacky way to store extra info in the objects.
+        // add box to the scene
         this.add(box);
         return box;
     }
 
-    addLights()
-    {
-         // add light and position it above and between camera and a prop
-         const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-         light.position.set(0, 5, 1);
- 
-         // add light to the scene
-         this.add(light);
+    addFloor() {
+        this.addBox(0xFFFFFF, new THREE.Vector3(0, 0, 0), new THREE.Vector3(10, 0.1, 10))
     }
 
-    // todo: remove one of them loadAndAddObjectAsync or loadAndAddObject
-    async loadAndAddObjectAsync()
-    {   
-        const gltf = await gltfLoader.loadAsync('assets/Wooden_Table.glb');
-        const model = gltf.scene;
-        model.position.set(0, 0, -1);
-        model.scale.set(3,3,3);
-        this.add( model );
-    } 
+    addLights() {
 
-    loadAndAddObject()
-    {   
-        const scene = this; //todo: reconsider, passed scene to a callback function some other way
-        gltfLoader.load('assets/Wooden_Table.glb', 
-          function (gltf){ 
-              const model = gltf.scene;
-              model.position.set(0, 0, -1);
-              model.scale.set(3,3,3);
-              scene.add( model );
-          },
-        
-          // called while loading is progressing
-          function ( xhr ) {
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-          },
+        this.add(new THREE.AmbientLight(0xffffff, 0.5));
 
-          // called when loading has errors
-          function ( error ) {
-            console.log( 'An error happened' + error );
-          }
-        )
+        // add light and position it above and between camera and a prop
+        const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+        light.position.set(0, 5, 1);
+        //light.castShadow = true;
+        // add light to the scene
+        this.add(light);
+    }
+
+
+    async loadObjectAsync(onLoaded: (model: THREE.Group | null) => void) {
+        const gltf = gltfLoader.loadAsync(gltTablePath);
+        gltf.then((result) => {
+            const model = result.scene;
+            model.position.set(0, 0, -1);
+            model.scale.set(3, 3, 3);
+            onLoaded(model);
+        })
+
+        gltf.catch((error) => {
+            console.log('Loading ' + gltTablePath + ' resulted in error: ' + error);
+            onLoaded(null);
+        })
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,124 +1,155 @@
 import './style.css'  // todo: remove?
 import * as THREE from 'three';
 import DemoScene from './DemoScene';
+
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { TransformControls } from 'three/addons/controls/TransformControls.js';
 
-
-// window params
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-// const params
-const fieldOfView = 75; // todo: play with it: Unity uses 60
-const aspectRatio = width / height;
-const nearPlane = 0.1;
-const farPlane = 100;
-
+// predefine colors for readability
 const colorLightBlue: THREE.ColorRepresentation = 0xADD8E6;
 const colorRed: THREE.ColorRepresentation = 0xFF0000;
+
+// predefine camera params for readability
+const fieldOfView = 60; // todo: play with it: Unity uses 60
+const aspectRatio = 1; // default, will be set to proper value in Init
+const nearPlane = 0.1;
+const farPlane = 100;
 
 // create global variables
 const renderer = new THREE.WebGLRenderer();
 const mainCamera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
 const scene = new DemoScene();
-const cameraControls = new OrbitControls( mainCamera, renderer.domElement );  // controls to move camera and look around
+const cameraControls = new OrbitControls(mainCamera, renderer.domElement);  // controls to move camera and look around
 
-// array to story objects we want to check against during recast
-let selectableObjects: THREE.Object3D[] = [];
+// array to store objects we want to check against during recast
+let selectableObjects: THREE.Object3D[] = []; // todo: make sure to add the object loaded from glb
 
 // Transform Controls for Gizmos
 const transformControls = new TransformControls(mainCamera, renderer.domElement);
 
 //Handling selection of an object
 const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
-const markerProp = scene.addBox(colorRed, new THREE.Vector3(0,0,0), new THREE.Vector3(0.01,10.0,0.01));
+const mouseCoords = new THREE.Vector2();
+const markerProp = scene.addBox(colorRed, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0.01, 10.0, 0.01)); // todo: .computeBoundingBox ()  can use it highlight selected mesh instead of using marker 
 
-function Init()
-{
-  // init renderer and set it to cover whole page
+function onLoadedObject(model: (THREE.Group | null)) {
+  if (model) {
+    selectableObjects.push(model);
+    scene.add(model);
+  }
+}
+
+function handleWindowSize() {
+  // window params
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
   renderer.setSize(width, height);
+  mainCamera.aspect = width / height;
+  mainCamera.updateProjectionMatrix();
+}
+
+// sets up most of the things that need to be setup only once in after loading
+function init() {
+  handleWindowSize();
+
+  // init renderer and set it to cover whole page
   renderer.setClearColor(colorLightBlue);
+  renderer.shadowMap.enabled = true
   document.body.appendChild(renderer.domElement); // todo: what exactly does it do? nothing is drawn if this is commented
 
   // init camera
-  mainCamera.position.set(0, 2, 4);    
+  mainCamera.position.set(0, 2, 4);
   mainCamera.lookAt(0, 0, 0);
-  
+
+  cameraControls.enablePan = false;
+  cameraControls.maxPolarAngle = Math.PI / 2;
+  cameraControls.enableDamping = true;
+
   // setup for the scene
+  scene.background = new THREE.Color(0xa8def0);
   scene.addLights();
   scene.addFloor();
-  
-  // loading and adding table
-  scene.loadAndAddObjectAsync();
 
+  // loading and adding table
+  scene.loadObjectAsync(onLoadedObject);
+  
   // setup gizmo
   transformControls.setMode('translate');
   scene.add(transformControls.getHelper());
 
   // add mesh prop
-  const box = scene.addBox(colorRed, new THREE.Vector3(2,2,-3), new THREE.Vector3(1,1,1));
+  const box = scene.addBox(colorRed, new THREE.Vector3(2, 2, -3), new THREE.Vector3(1, 1, 1));
   scene.add(box);
   selectableObjects.push(box);
 
   // add debug marker as helper to check recast results 
   scene.add(markerProp);
+  requestAnimationFrame(loop);
+}
+
+// checks intersation every frame using mouseCoords updated base on input 
+function updateSelection()  // todo: it's still very slugish
+{
+  // update the picking ray with the camera and pointer position
+  raycaster.setFromCamera(mouseCoords, mainCamera);
+  const intersectedObjects = raycaster.intersectObjects(selectableObjects, true);
+
+  if (intersectedObjects.length > 0) {
+    const hitObject = intersectedObjects[0].object;
+    const pos = hitObject.position;
+    markerProp.position.set(pos.x, pos.y, pos.z);
+  }
+  else {
+    markerProp.position.set(0, 0, 0);
+  }
 }
 
 // main loop of application
-function loop() 
-{
-  // todo: considering removing. it worked without update from docs: "required if controls.enableDamping or controls.autoRotate are set to true"
+function loop() {
   cameraControls.update();
-
-   // pass scene and camera to renderer and render the scene
+  updateSelection();
+  // pass scene and camera to renderer and render the scene
   renderer.render(scene, mainCamera);
   // Browser handles timing and throttling
   requestAnimationFrame(loop);
 }
 
-// >>>>> start of application <<<<<
-Init();
-requestAnimationFrame(loop);
-
 // event handlers
-function onPointerMove( event: any ) {  //todo: would be nice to get rid of any
+function onMouseMove(event: any)  //todo: would be nice to get rid of any
+{
+  // calculate mouseCoor position in normalized device coordinates (-1 to +1) for both components
+  mouseCoords.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouseCoords.y = - (event.clientY / window.innerHeight) * 2 + 1;
+}
 
-	// calculate pointer position in normalized device coordinates (-1 to +1) for both components
-  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-  
-  // update the picking ray with the camera and pointer position
-	raycaster.setFromCamera( pointer, mainCamera );
-  const intersectedObjects = raycaster.intersectObjects( selectableObjects, true);
- 
-  if( intersectedObjects.length > 0)
-  {
-    const hitObject = intersectedObjects[ 0 ].object;
-    const pos = hitObject.position;
-    markerProp.position.set(pos.x, pos.y, pos.z);
-    transformControls.attach(hitObject);
+//let selectedObject: [boolean, THREE.Object3D];  // todo: find something like optional<>
+
+function onClick(event: any) {
+
+  mouseCoords.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouseCoords.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(mouseCoords, mainCamera);
+  const hitObjects = raycaster.intersectObjects(selectableObjects, true);
+
+  if (hitObjects.length > 0) {
+    const selectedObject = hitObjects[0].object;
+    transformControls.attach(selectedObject);
+    //selectedObject = [true, hitObjects[0].object];
   }
-  else
-  {
-    markerProp.position.set(0, 0, 0);
+  else {
     transformControls.detach();
+    //selectedObject[0] = false;
   }
 }
 
-function onWindowResize()
-{
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-
-  mainCamera.aspect = width / height;
-  mainCamera.updateProjectionMatrix();
-
-  renderer.setSize(width, height);
+function onWindowResize() {
+  handleWindowSize();
 }
 
 // listeners
-window.addEventListener( 'resize', onWindowResize );
-window.addEventListener( 'pointermove', onPointerMove );
-
+window.addEventListener('resize', onWindowResize);
+window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('click', onClick);
+window.onload = init;
